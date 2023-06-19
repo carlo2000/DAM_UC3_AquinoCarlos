08d692b10b52c33a2a388a7cb33dd6d7f6968664
CarlosAquino <1913110305@untels.edu.pe>
2023-06-19 17:31:16 -0500

    Segundo cambio

--- app/src/main/java/com/example/dam_uc3_carlosaquino/MainActivity.java
+++ app/src/main/java/com/example/dam_uc3_carlosaquino/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.dam_uc3_carlosaquino;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +20,28 @@ public class MainActivity extends AppCompatActivity {
         Button btnRegistrar;
         btnRegistrar = findViewById(R.id.btnRegistrate);
         btnRegistrar.setOnClickListener(view -> Toast.makeText(this,"Se envio sus datos",Toast.LENGTH_LONG).show());
+
+        Spinner spinnerLanguages=findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.Sexo, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerLanguages.setAdapter(adapter);
+
+
+
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+
+    }
+
+
+
+
 }
